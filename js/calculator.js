0f121852b69da4d1e1e4aaee2ffa7f3d030da6bb
Bentroy <bentroy006@gmail.com>
2025-01-09 23:27:55 +0100

    fixed the problem with the javascript, i put queryselector instead of querySelectorAll

--- js/calculator.js
+++ js/calculator.js
@@ -1,19 +1,19 @@
 const display = document.querySelector('#display');
-const buttons = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
 buttons.forEach((item) => {
     item.onclick = () => {
         if (item.id == 'clear') {
             display.innerText = '';
-        }else if (item.id == 'backspace') {
+        } else if (item.id == 'backspace') {
             let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length - 1);        
-        }else if (display.innerText !='' && item.id == 'equal') {
-            display.innerText = eval(display.innerText);          
-        }else if (display.innerText =='' && item.id == 'equal') {
+            display.innerText = string.substr(0, string.length - 1);
+        } else if (display.innerText != '' && item.id == 'equal') {
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == '' && item.id == 'equal') {
             display.innerText = 'Empty';
             setTimeout(() => (display.innerText = ''), 2000);
-        }else{
+        } else {
             display.innerText += item.id;
         }
     }
@@ -24,7 +24,7 @@ const themeToggleBtn = document.querySelector('.theme-toggler');
 const calculator = document.querySelector('.calculator');
 const toggleIcon = document.querySelector('.toggler-icon');
 let isDark = true;
-themeToggleBtn.onclick = () =>{
+themeToggleBtn.onclick = () => {
     calculator.classList.toggle('dark');
     themeToggleBtn.classList.toggle('active');
     isDark = !isDark;
